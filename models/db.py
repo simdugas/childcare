7253d86b72b1b6723133505a7fd52aa8cabf612f
Richard Panell <richard.pannell@gmail.com>
2013-10-10 14:39:57 -0300

    Update db.py
    Adding schema for database.

--- models/db.py
+++ models/db.py
@@ -1,83 +1,54 @@
-# -*- coding: utf-8 -*-
+db = DAL('sqlite://storage.sqlite')
 
-#########################################################################
-## This scaffolding model makes your app work on Google App Engine too
-## File is released under public domain and you can use without limitations
-#########################################################################
-
-## if SSL/HTTPS is properly configured and you want all HTTP requests to
-## be redirected to HTTPS, uncomment the line below:
-# request.requires_https()
-
-if not request.env.web2py_runtime_gae:
-    ## if NOT running on Google App Engine use SQLite or other DB
-    db = DAL('sqlite://storage.sqlite',pool_size=1,check_reserved=['all'])
-else:
-    ## connect to Google BigTable (optional 'google:datastore://namespace')
-    db = DAL('google:datastore')
-    ## store sessions and tickets there
-    session.connect(request, response, db=db)
-    ## or store session in Memcache, Redis, etc.
-    ## from gluon.contrib.memdb import MEMDB
-    ## from google.appengine.api.memcache import Client
-    ## session.connect(request, response, db = MEMDB(Client()))
-
-## by default give a view/generic.extension to all actions from localhost
-## none otherwise. a pattern can be 'controller/function.extension'
-response.generic_patterns = ['*'] if request.is_local else []
-## (optional) optimize handling of static files
-# response.optimize_css = 'concat,minify,inline'
-# response.optimize_js = 'concat,minify,inline'
-
-#########################################################################
-## Here is sample code if you need for
-## - email capabilities
-## - authentication (registration, login, logout, ... )
-## - authorization (role based authorization)
-## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
-## - old style crud actions
-## (more options discussed in gluon/tools.py)
-#########################################################################
-
-from gluon.tools import Auth, Crud, Service, PluginManager, prettydate
+from gluon.tools import *
 auth = Auth(db)
-crud, service, plugins = Crud(db), Service(), PluginManager()
+auth.define_tables()
+crud = Crud(db)
 
-## create all tables needed by auth if not custom tables
-auth.define_tables(username=False, signature=False)
+db.define_table('agencies',
+    Field('childcare_provider_id', 'integer', requires=IS_NOT_EMPTY()),
+    Field('facility_type_id', 'reference facility_types'),
+    Field('name', unique=True, requires=IS_NOT_EMPTY()),
+    Field('street', requires=IS_NOT_EMPTY()),
+    Field('county', requires=IS_NOT_EMPTY()),
+    Field('email', requires=IS_NOT_EMPTY()),
+    Field('min_age', 'integer', requires=IS_NOT_EMPTY()),
+    Field('max_age', 'integer', requires=IS_NOT_EMPTY()),
+    Field('capacity', 'integer', requires=IS_NOT_EMPTY()),
+    Field('annual_inspection_date', 'datetime', requires=IS_NOT_EMPTY()),
+    Field('unannounced_inspection_date', 'datetime'),
+    Field('in_compliance', 'boolean'),
+    Field('created_on', 'datetime', default=request.now),
+    Field('created_by', 'reference auth_user', default=auth.user_id),
+    format='%(name)s')
+    
+db.define_table('facility_types',
+    Field('name', unique=True, requires=IS_NOT_EMPTY()),
+    Field('created_on', 'datetime', default=request.now),
+    Field('created_by', 'reference auth_user', default=auth.user_id),
+    format='%(name)s') 
 
-## configure email
-mail = auth.settings.mailer
-mail.settings.server = 'logging' or 'smtp.gmail.com:587'
-mail.settings.sender = 'you@gmail.com'
-mail.settings.login = 'username:password'
+db.define_table('phone_numbers',
+    Field('agency_id', 'references agencies'),
+    Field('phone_number_type_id', 'references phone_number_types'),
+    Field('phone_number', requires=IS_NOT_EMPTY()),
+    Field('created_on', 'datetime', default=request.now),
+    Field('created_by', 'reference auth_user', default=auth.user_id),
+    format='%(phone_number)s') 
 
-## configure auth policy
-auth.settings.registration_requires_verification = False
-auth.settings.registration_requires_approval = False
-auth.settings.reset_password_requires_verification = True
+db.define_table('phone_number_types',
+    Field('name', requires=IS_NOT_EMPTY()),
+    Field('created_on', 'datetime', default=request.now),
+    Field('created_by', 'reference auth_user', default=auth.user_id),
+    format='%(name)s') 
 
-## if you need to use OpenID, Facebook, MySpace, Twitter, Linkedin, etc.
-## register with janrain.com, write your domain:api_key in private/janrain.key
-from gluon.contrib.login_methods.rpx_account import use_janrain
-use_janrain(auth, filename='private/janrain.key')
+db.define_table('program_types',
+    Field('agency_id', 'references agencies'),
+    Field('name', 'references phone_number_types', requires=IS_NOT_EMPTY()),
+    Field('created_on', 'datetime', default=request.now),
+    Field('created_by', 'reference auth_user', default=auth.user_id),
+    format='%(name)s')
 
-#########################################################################
-## Define your tables below (or better in another model file) for example
-##
-## >>> db.define_table('mytable',Field('myfield','string'))
-##
-## Fields can be 'string','text','password','integer','double','boolean'
-##       'date','time','datetime','blob','upload', 'reference TABLENAME'
-## There is an implicit 'id integer autoincrement' field
-## Consult manual for more options, validators, etc.
-##
-## More API examples for controllers:
-##
-## >>> db.mytable.insert(myfield='value')
-## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
-## >>> for row in rows: print row.id, row.myfield
-#########################################################################
-
-## after defining tables, uncomment below to enable auditing
-# auth.enable_record_versioning(db)
+db.agencies.facility_type_id.requires = IS_IN_DB(db, db.facility_types.id, '%(name)s')
+db.phone_numbers.phone_number_type_id.requires = IS_IN_DB(db, db.phone_number_types.id, '%(name)s')
+db.program_types.agency_id.requires = IS_IN_DB(db, db.agencies.id)
