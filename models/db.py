266c4c3ff743e5f9901372848e76dff4750c3e2a
simdugas <simdugas@gmail.com>
2013-10-18 15:11:35 -0300

    added geometry

--- models/db.py
+++ models/db.py
@@ -4,11 +4,6 @@ auth = Auth(db)
 auth.define_tables()
 crud = Crud(db)
 
-db.define_table('facility_types',
-    Field('name', unique=True, requires=IS_NOT_EMPTY()),
-    Field('created_on', 'datetime', default=request.now),
-    Field('created_by', 'reference auth_user', default=auth.user_id),
-    format='%(name)s') 
 
 db.define_table('agencies',
     Field('childcare_provider_id', 'integer', requires=IS_NOT_EMPTY(), unique=True),
@@ -25,7 +20,21 @@ db.define_table('agencies',
     Field('in_compliance', 'boolean'),
     Field('created_on', 'datetime', default=request.now),
     Field('created_by', 'reference auth_user', default=auth.user_id),
-    format='%(name)s')    
+    format='%(name)s')
+    
+db.define_table('facility_types',
+    Field('name', unique=True, requires=IS_NOT_EMPTY()),
+    Field('created_on', 'datetime', default=request.now),
+    Field('created_by', 'reference auth_user', default=auth.user_id),
+    format='%(name)s') 
+
+db.define_table('phone_numbers',
+    Field('agency_id', 'reference agencies'),
+    Field('phone_number_type_id', 'references phone_number_types'),
+    Field('phone_number', requires=IS_NOT_EMPTY()),
+    Field('created_on', 'datetime', default=request.now),
+    Field('created_by', 'reference auth_user', default=auth.user_id),
+    format='%(phone_number)s') 
 
 db.define_table('phone_number_types',
     Field('name', requires=IS_NOT_EMPTY()),
@@ -33,26 +42,18 @@ db.define_table('phone_number_types',
     Field('created_by', 'reference auth_user', default=auth.user_id),
     format='%(name)s') 
 
-db.define_table('phone_numbers',
-    Field('agency_id', 'references agencies'),
-    Field('phone_number_type_id', 'references phone_number_types'),
-    Field('phone_number', requires=IS_NOT_EMPTY()),
-    Field('created_on', 'datetime', default=request.now),
-    Field('created_by', 'reference auth_user', default=auth.user_id),
-    format='%(phone_number)s') 
-
 db.define_table('program_types',
-    Field('agency_id', 'references agencies'),
-    Field('name', 'references phone_number_types', requires=IS_NOT_EMPTY()),
+    Field('agency_id', 'reference agencies'),
+    Field('name', 'reference phone_number_types', requires=IS_NOT_EMPTY()),
     Field('created_on', 'datetime', default=request.now),
     Field('created_by', 'reference auth_user', default=auth.user_id),
     format='%(name)s')
-
-db.agencies.facility_type_id.requires = IS_IN_DB(db, db.facility_types.id, '%(name)s')
-db.phone_numbers.phone_number_type_id.requires = IS_IN_DB(db, db.phone_number_types.id, '%(name)s')
-db.program_types.agency_id.requires = IS_IN_DB(db, db.agencies.id)
 
 db.define_table('geolocation',
     Field('agency_id', 'references agencies'),
     Field('location', 'geometry()')    
     )
+
+db.agencies.facility_type_id.requires = IS_IN_DB(db, db.facility_types.id, '%(name)s')
+db.phone_numbers.phone_number_type_id.requires = IS_IN_DB(db, db.phone_number_types.id, '%(name)s')
+db.program_types.agency_id.requires = IS_IN_DB(db, db.agencies.id)
