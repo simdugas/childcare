6f42671bdef273f022e578aeae660bdbe9d14f34
simdugas <simdugas@gmail.com>
2013-10-18 14:58:26 -0300

    Added geometry

--- models/db.py
+++ models/db.py
@@ -1,7 +1,14 @@
 from gluon.tools import *
+from gluon.dal import DAL, Field, geoPoint, geoLine, geoPolygon
 auth = Auth(db)
 auth.define_tables()
 crud = Crud(db)
+
+db.define_table('facility_types',
+    Field('name', unique=True, requires=IS_NOT_EMPTY()),
+    Field('created_on', 'datetime', default=request.now),
+    Field('created_by', 'reference auth_user', default=auth.user_id),
+    format='%(name)s') 
 
 db.define_table('agencies',
     Field('childcare_provider_id', 'integer', requires=IS_NOT_EMPTY(), unique=True),
@@ -18,10 +25,10 @@ db.define_table('agencies',
     Field('in_compliance', 'boolean'),
     Field('created_on', 'datetime', default=request.now),
     Field('created_by', 'reference auth_user', default=auth.user_id),
-    format='%(name)s')
-    
-db.define_table('facility_types',
-    Field('name', unique=True, requires=IS_NOT_EMPTY()),
+    format='%(name)s')    
+
+db.define_table('phone_number_types',
+    Field('name', requires=IS_NOT_EMPTY()),
     Field('created_on', 'datetime', default=request.now),
     Field('created_by', 'reference auth_user', default=auth.user_id),
     format='%(name)s') 
@@ -34,12 +41,6 @@ db.define_table('phone_numbers',
     Field('created_by', 'reference auth_user', default=auth.user_id),
     format='%(phone_number)s') 
 
-db.define_table('phone_number_types',
-    Field('name', requires=IS_NOT_EMPTY()),
-    Field('created_on', 'datetime', default=request.now),
-    Field('created_by', 'reference auth_user', default=auth.user_id),
-    format='%(name)s') 
-
 db.define_table('program_types',
     Field('agency_id', 'references agencies'),
     Field('name', 'references phone_number_types', requires=IS_NOT_EMPTY()),
@@ -50,3 +51,8 @@ db.define_table('program_types',
 db.agencies.facility_type_id.requires = IS_IN_DB(db, db.facility_types.id, '%(name)s')
 db.phone_numbers.phone_number_type_id.requires = IS_IN_DB(db, db.phone_number_types.id, '%(name)s')
 db.program_types.agency_id.requires = IS_IN_DB(db, db.agencies.id)
+
+db.define_table('geolocation',
+    Field('agency_id', 'references agencies'),
+    Field('location', 'geometry()')    
+    )
