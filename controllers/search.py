18d3400959d06515552e8bf41a34adf5f77ec08f
simdugas <simdugas@gmail.com>
2013-10-20 15:40:49 -0300

    Added connection to Google Maps API and a test Map

--- controllers/search.py
+++ controllers/search.py
@@ -1,5 +1,13 @@
+#Defaults to a search form
 def index():
+    #Search form
     form = FORM(DIV('Where am I'), DIV(INPUT(_name='location')),
         DIV('Program Type'), DIV(INPUT(_name='program-type')),
         DIV('Age'), DIV(INPUT(_name='age')), DIV(INPUT(_type='submit')))    
     return dict(form=form)
+
+#Shows the search results
+def result():
+    #Testing the Google Maps API
+    locations = db['geolocation'].select()
+    return dict()
