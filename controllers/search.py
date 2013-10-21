b713e18be5577bce98f1a6897ea9f970acf542f7
simdugas <simdugas@gmail.com>
2013-10-21 17:25:58 -0300

    added markers to the map

--- controllers/search.py
+++ controllers/search.py
@@ -12,6 +12,24 @@ def result():
     #Testing the Google Maps API
     agencies = db(
         (db.geolocation.agency_id==db.agencies.id)
-    ).select().as_list()
+    ).select()
     
-    return dict(agencies=json.dumps(agencies))
+    # Create agency list for view
+    agency_list = []
+    for agency in agencies:
+        add = dict()
+        add['id'] = agency.agencies.id
+        add['childcare_provider_id'] = agency.agencies.childcare_provider_id
+        add['name'] = agency.agencies.name
+        add['street'] = agency.agencies.street
+        add['county'] = agency.agencies.county
+        add['email'] = agency.agencies.email
+        add['min_age'] = agency.agencies.min_age
+        add['max_age'] = agency.agencies.max_age
+        add['capacity'] = agency.agencies.capacity
+        #add['annual_inspection_date'] = agency.agencies.annual_inspection_date.isoformat()
+        add['in_compliance'] = agency.agencies.in_compliance
+        add['geolocation'] = agency.geolocation.location
+        agency_list.append(add)
+    
+    return dict(agencies=json.dumps(agency_list))
