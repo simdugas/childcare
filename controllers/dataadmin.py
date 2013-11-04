e4db951e50e491d324219630a38bea08cf19e61c
Richard Pannell <richard.pannell@gmail.com>
2013-11-04 17:09:17 -0400

    adding cleaned up functionality to data import

--- controllers/dataadmin.py
+++ controllers/dataadmin.py
@@ -2,6 +2,8 @@ import os
 import csv
 import json
 import re
+import math
+import datetime
 
 def index():
     cols = []
@@ -18,29 +20,8 @@ def index():
                     #set columns first time through
                     cols = row
                 else:
-
-
-                    #
-                    #insert facility type
-                    #
-                    
-                    #check to see if the facility type exists
-                    facility_type = db(db.facility_types.name==row[2]).select().first()
-                    ftid = ''
-
-                    #insert the facility type if id doesn't exist
-                    if not facility_type:
-                        ftid = db.facility_types.insert(name=row[2])
-
-
-                    #
-                    # insert agency
-                    #
-
-                    #default age min and max to 0    
-                    ages = {'min':0, 'max':0}
-                    if row[8] != '':    
-                        ages = minMaxAges(row[8]);
+                    ftid = insertFacilityType(row[2])
+                    ages = minMaxAges(row[8].strip());
                     
                     #check to see if an agency with this name exists
                     agency_test = db.agencies(name=row[1])
@@ -57,61 +38,96 @@ def index():
                                                        min_age = ages['min'],
                                                        max_age = ages['max'],
                                                        capacity = row[9] if row[9].strip() >= 0 else 0,
-                                                       in_compliance = 0 if row[12] == 'No' else 1
+                                                       in_compliance = 0 if row[12] == 'No' else 1,
+                                                       annual_inspection_date = parseDate(row[10]),
+                                                       annual_inspection_date = parseDate(row[11])
                                             
                                             
                                                        )               
 
-
-                    #
-                    #Ok the point insert should go here
-                    #https://groups.google.com/forum/#!topic/web2py/feh1ksfdkGk
+                    if agency_id:
+                        insertAgencyLocation(row[14], row[15], agency_id);
+                        insertProgramTypes(row[7], agency_id)
                     
-                    if row[14] != '' and row[15] != '' and agency_id != '':
-                        lat = row[14]
-                        lng = row[15]
-                        db.geolocation.insert(
-                                agency_id=int(agency_id), 
-                                location="POINT (" + lat + " " + lng + "2)"
-                                )
-
-                    #split the program types to a collection
-                    program_types = row[7].split(',')
-                    program_type_id = ''    
-                    if program_types:
-                        for program_type in program_types:
-                            
-                            #test to see if program type exists
-                            p_type = db.program_types(name=program_type)
-                            
-                            #if program type does not exist then create it
-                            if not p_type:
-                                program_type_id = db.program_types.insert(name = program_type)
-                            else:
-                                program_type_id = p_type.id
-                            
-                            #create record to join program types with agency
-                            db.agency_program_types.insert(agency_id = int(agency_id), program_type_id = int(program_type_id))
-
-    return dict(form=form)
+    return locals()
 
 
 def minMaxAges(ages_string):
+    naPattern = re.compile('N/A')
     pattern = re.compile('to')
     minmax = []
 
-    if pattern.search(ages_string):
-        minmax = ages_string.split('to')
-    else: 
-        minmax = ages_string.split('-')
-    
-    return dict(min = ageFor(minmax[0]), 
-                max = ageFor(minmax[1])
-                )
+    min = 0
+    max = 0
+
+    if not naPattern.search(ages_string):
+        if pattern.search(ages_string):
+            minmax = ages_string.split('to')
+        else: 
+            minmax = ages_string.split('-')
+        min = ageFor(minmax[0])
+        max = ageFor(minmax[1])
+
+    return locals()
 
 def ageFor(age_string):
+    
     ageType = age_string.strip().split()
+    age = 0
+    
     if not ageType[0]:
         return 0
     else:
-        return int(ageType[0]) * 12 if ageType[1] == 'years' else ageType[0]
+        try:
+            age = int(ageType[0])
+        except ValueError:
+            age = math.floor(float(ageType[0]))
+        else:
+            age = 0
+            
+    return (age * 12 if ageType[1] == 'years' else age)
+
+def insertFacilityType(facility_type_name):
+    #check to see if the facility type exists
+    facility_type = db(db.facility_types.name==facility_type_name).select().first()
+    ftid = ''
+    #insert the facility type if id doesn't exist
+    if not facility_type:
+        ftid = db.facility_types.insert(name=facility_type_name)
+    else:
+        ftid = facility_type.id
+        
+    return ftid;
+
+def insertAgencyLocation(lat, lng, agency_id):
+    geolocation_id = ''
+    if lat and lng:
+        geolocation_id = db.geolocation.insert(
+            agency_id=int(agency_id), 
+            location="POINT (" + lat + " " + lng + "2)"
+            )
+    return geolocation_id
+
+def insertProgramTypes(program_types_string, agency_id):
+#split the program types to a collection
+    program_types = program_types_string.split(',')
+    program_type_id = ''    
+    if program_types:
+        for program_type in program_types:
+            #test to see if program type exists
+            exists = db.program_types(name=program_type)
+            
+            #if program type does not exist then create it
+            if not exists:
+                program_type_id = db.program_types.insert(name = program_type)
+            else:
+                program_type_id = exists.id
+                
+    db.agency_program_types.insert(agency_id = agency_id, 
+                                   program_type_id = program_type_id
+                                   )
+
+def parseDate(date_string):
+    return ('' if date_string == 'N/A' else date_string)
+
+
